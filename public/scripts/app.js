8042d2e6091e9e0594818ed162e024718e2e03d3
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-04 16:12:36 -0400

    add-logout-using-jQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 // Client facing scripts here
+$(document).ready(function(){
+    alert('jQ loaded');
+    $logoutBtn = $('[href="/logout"]');
+    $logoutBtn.click((event)=>{
+        event.preventDefault();
+        $.post('/logout');
+    })
+});
