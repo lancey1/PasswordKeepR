e47f6e1bc5be81a0833c655a74fef3246e41973c
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-04 16:14:56 -0400

    add-comments-about-jQuery-logout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,13 @@
 // Client facing scripts here
 $(document).ready(function(){
-    alert('jQ loaded');
+    
+    //* Logout should be a post action, however HTML <a> cannot send a post request, So we use jQuery to do the action.
     $logoutBtn = $('[href="/logout"]');
+
     $logoutBtn.click((event)=>{
         event.preventDefault();
         $.post('/logout');
     })
+
+    
 });
