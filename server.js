0c7075444f5d8bb77ab6425309502d63b29203ef
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-05 15:15:48 -0400

    add-comments-to-serve.js

--- server.js
+++ server.js
@@ -11,19 +11,12 @@ const morgan = require("morgan");
 const cookieSession = require('cookie-session');
 const pool = require('./lib/db');
 
-
-// PG database pool/connection setup
-// pool.connect();
-
-
-// const { Pool } = require("pg");
-// const dbParams = require("./lib/db.js");
-// const db = new Pool(dbParams);
-
-
+const usersRoutes = require("./routes/users");
+const NEDRoutes = require('./routes/new_edit_delete');
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
 app.use(morgan("dev"));
 //* set up view-engine and parser.
 app.set("view engine", "ejs");
@@ -45,29 +38,13 @@ app.use(cookieSession({
   keys: ['key1', 'key2'],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
+//* Check every incoming request's session.
+app.use(checkSessionMiddleware);
 
-
-const usersRoutes = require("./routes/users");
-const NEDRoutes = require('./routes/new_edit_delete');
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-/* app.use((req, res, next) => {
-  if (req.session['user_id'] !== '' || !req.session['user_id']) {
-    //todo  - Check if user exist, Query from database.
-    for (let key in users) {
-      if (key === req.session['user_id']) {
-        res.locals.useremail = users[key]['email'];
-        res.locals.isAuth = true;
-      };
-    }
-  }
-  next();
-}); */
 app.get("/", (req, res) => {
   res.render("main");
 });
-app.use(checkSessionMiddleware);
+
 app.use(usersRoutes);
 app.use(NEDRoutes);
 
