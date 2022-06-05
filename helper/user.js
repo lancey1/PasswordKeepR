ccec86a8469ee6e5f83347dc08c78012ce0b8efd
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-04 21:17:47 -0400

    add-user-login

--- helper/user.js
+++ helper/user.js
@@ -1,4 +1,5 @@
 const { queryUserInfoByEmail } = require('./queries');
+const bcrypt = require('bcryptjs');
 
 const signupCheck = async function (email, password, confirm_password) {
     let user;
@@ -13,12 +14,18 @@ const signupCheck = async function (email, password, confirm_password) {
     if (password.trim() != confirm_password.trim()) return 'Passwords does not match.';
 }
 
-
-const loginCheck = function (email, password, confirm_password) {
-
-
+const loginCheck = async function (email, password) {
+    try {
+        const user = await queryUserInfoByEmail(email);
+        if (!user) {
+            return 'User does not exist.'
+        }
+        let passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) return "Please check your credentials."
+    } catch (error) {
+        throw error;
+    }
 }
-
 
 module.exports = {
     signupCheck,
--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,25 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { insertUser, queryUserInfoByEmail, queryInfoByUserId, queryInfoByWebTypeAndUserId } = require('../helper/queries');
-const { signupCheck } = require('../helper/user')
-
-
-
+const { signupCheck, loginCheck } = require('../helper/user')
 
 router.get('/login', (req, res) => {
   res.render('login');
 })
 
-router.post('/login', (req, res) => {
-
-  res.send(JSON.stringify(req.body));
+router.post('/login', async (req, res) => {
+  let { email, password } = req.body;
+  let msg;
+  try {
+    msg = await loginCheck(email, password);
+    if (msg) {
+      return res.render('login', { warning: msg, email: email });
+    }
+  } catch (error) {
+    throw "login post:" + error['message'];
+  }
+  req.session.email = email;
+  return res.render('main');
 })
 
 router.get('/signup', (req, res) => {
@@ -35,10 +42,10 @@ router.post('/signup', async (req, res) => {
     let msg = await signupCheck(email, password, confirm_password);
     console.log(msg);
     if (msg) {
-      return res.render('signup', { warning: msg, organization: organization, email:email, username:username });
+      return res.render('signup', { warning: msg, organization: organization, email: email, username: username });
     }
   } catch (error) {
-    throw error['message'];
+    throw "signup post:" + error['message'];
   }
 
   //* Hash user password then store it.
@@ -59,13 +66,6 @@ router.post('/logout', (req, res) => {
 })
 
 router.get('/main', async (req, res) => {
-  // try {
-  //   const result = await queryInfoByUserId(2);
-  //   res.send(result);
-  // } catch (error) {
-
-  //   throw error['message'];
-  // }
   res.render('main');
 })
 
