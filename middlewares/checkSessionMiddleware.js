930d5c7edbd04a2e49d0ea4573f783c109f6717b
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-04 23:30:02 -0400

    update-check-session-middleware

--- middlewares/checkSessionMiddleware.js
+++ middlewares/checkSessionMiddleware.js
@@ -4,9 +4,11 @@ const { queryUserInfoByEmail } = require('../helper/queries');
 const checkRequestSessionInfo = async (req, res, next) => {
     if (req.session['email']) {
         try {
+            //* Fetch user info if user had a email cookie.
             let user = await queryUserInfoByEmail(req.session['email']);
             console.log('in check session middleware ', user);
             res.locals.user = user;
+            res.locals.isAuth = true;
         } catch (error) {
             throw error['message'];
         }
