76eb1142b4bdddb30fee2cc8cfefa4beb073f74d
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-06 19:21:58 -0400

    delete-jQuery-lines

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,6 @@
 // Client facing scripts here
 $(document).ready(function () {
 
-    const $webListURL = $('.webinfo-card-type');
-    $webListURL.click(function() {
-        const $webInfo = $(this).siblings('div');
-        $webInfo.slideToggle('slow');
-    })
 
 
 });
