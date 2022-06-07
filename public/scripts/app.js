aa5b6892f34febc73199c22cb6afd87b822973ef
vagrant <lanceyu619@gmail.com>
2022-06-07 19:26:56 +0000

    add copy button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,23 @@
-// Client facing scripts here
-$(document).ready(function () {
+function copyButton() {
+  /* Get the text field */
+  let copyText = document.getElementById("username-pswd-info-password");
 
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
 
-});
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+  }
+
+  function showPassword() {
+  let x = document.getElementById("username-pswd-info-password");
+  if (x.type === "password") {
+  x.type = "text";
+  } else {
+  x.type = "password";
+  }
+}
