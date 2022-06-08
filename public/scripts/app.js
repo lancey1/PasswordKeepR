16476b7b582fcb398532e45c02de1994ff6ee05b
vagrant <lanceyu619@gmail.com>
2022-06-08 23:23:36 +0000

    add url submission verification

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  /* Get the text field */
   $(".copybutton").mousedown(function () {
     let copyText = $(this).val();
     console.log(copyText);
@@ -12,15 +11,58 @@ $(document).ready(function () {
 
   $(".username-pswd-info-password").mouseover(function () {
     $(this)
-      .closest('form')
-      .find('.username-pswd-info-password')
-      .attr('type', 'text');
+      .closest("form")
+      .find(".username-pswd-info-password")
+      .attr("type", "text");
   });
 
   $(".username-pswd-info-password").mouseout(function () {
     $(this)
-      .closest('form')
-      .find('.username-pswd-info-password')
-      .attr('type', 'password');
+      .closest("form")
+      .find(".username-pswd-info-password")
+      .attr("type", "password");
+  });
+});
+
+$(document).ready(function () {
+  $("#disabledsubmit").prop("disabled", true);
+
+  $("#urlsubmissionform").change(function () {
+    const $email = ($("#user_email").val());
+    const $url = ($("#website_url").val());
+    const $password = ($("#passwordlength").val());
+    $("#lowercase").val($("#lowercase").is(":checked") ? 1 : 0);
+    $("#uppercase").val($("#uppercase").is(":checked") ? 1 : 0);
+    $("#number").val($("#number").is(":checked") ? 1 : 0);
+    $("#specialchar").val($("#specialchar").is(":checked") ? 1 : 0);
+    const $lowercase = Number($("#lowercase").val());
+    const $uppercase = Number($("#uppercase").val());
+    const $numbers = Number($("#number").val());
+    const $special = Number($("#specialchar").val());
+
+    if ($("#passwordlength").val() <= 6 && $("#passwordlength").val() >= 48 && $("#passwordlength").length === 0) {
+      $("#disabledsubmit").prop("disabled", true);
+    }
+    if ($("#website_url").val().length === 0) {
+      $("#disabledsubmit").prop("disabled", true);
+    }
+    if ($("#user_email").val().length === 0) {
+      $("#disabledsubmit").prop("disabled", true);
+    }
+    if ($lowercase === 0 && $uppercase === 0 && $numbers === 0 && $special === 0) {
+      $("#disabledsubmit").prop("disabled", true);
+      }
+    if (
+    (
+    $("#passwordlength").val() >= 6 &&
+    $("#passwordlength").val() <= 48 &&
+    $("#website_url").val().length > 0 &&
+    $("#user_email").val().length > 0
+    ) &&
+    ($lowercase > 0 || $uppercase > 0 || $numbers > 0 || $special > 0)
+    )
+    {
+      $("#disabledsubmit").prop("disabled", false);
+    }
   });
 });
