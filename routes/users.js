cc4ea19fc9958922689bbd752a8efc2018a4cad9
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-09 16:00:27 -0400

    for-demo'

--- routes/users.js
+++ routes/users.js
@@ -57,21 +57,21 @@ router.post('/signup', async (req, res) => {
     //!
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-         to: email,
-         from: email,
-         templateId: "d-e9c8e759c12249028517d24ecaaa806a",
-         dynamic_template_data: {
-          name: username
-         }
-      };
-      sgMail.send(msg, (error, result) => {
-         if (error) {
-             console.log(error);
-         } else {
-          console.log('Email sent')
-        }
-      });
+    const msg = {
+      to: email,
+      from: "lanceyu1010@gmail.com",
+      templateId: "d-e9c8e759c12249028517d24ecaaa806a",
+      dynamic_template_data: {
+        name: username
+      }
+    };
+    sgMail.send(msg, (error, result) => {
+      if (error) {
+        console.log(error['message']);
+      } else {
+        console.log('Email sent')
+      }
+    });
     //!
     return res.redirect('/home');
   } catch (error) {
