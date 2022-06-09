e6d62613bb0e5bd3d0ac8f4a17d297da3c86031f
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-08 21:37:56 -0400

    add-copy-btn-to-list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     navigator.clipboard.writeText(copyText);
     // /* Alert the copied text */
     console.log("Copied the text: " + copyText);
-    alert("Copied the text: " + copyText);
+    // alert("Copied the text: " + copyText);
   });
 
   $(".username-pswd-info-password").mouseover(function () {
@@ -22,10 +22,7 @@ $(document).ready(function () {
       .find(".username-pswd-info-password")
       .attr("type", "password");
   });
-});
 
-$(document).ready(function () {
-  // $("#disabledsubmit").prop("disabled", true);
 
   const checkForm = function () {
     $("#lowercase").val($("#lowercase").is(":checked") ? 1 : 0);
@@ -72,43 +69,4 @@ $(document).ready(function () {
   $(".form-check-input").click(checkForm);
   $(".responsive-input").keyup(checkForm);
 
-  // $("#urlsubmissionform").change(function () {
-  //   const $email = ($("#user_email").val());
-  //   const $url = ($("#website_url").val());
-  //   const $password = ($("#passwordlength").val());
-  //   $("#lowercase").val($("#lowercase").is(":checked") ? 1 : 0);
-  //   $("#uppercase").val($("#uppercase").is(":checked") ? 1 : 0);
-  //   $("#number").val($("#number").is(":checked") ? 1 : 0);
-  //   $("#specialchar").val($("#specialchar").is(":checked") ? 1 : 0);
-  //   const $lowercase = Number($("#lowercase").val());
-  //   const $uppercase = Number($("#uppercase").val());
-  //   const $numbers = Number($("#number").val());
-  //   const $special = Number($("#specialchar").val());
-
-
-  //   if ($("#passwordlength").val() <= 6 || $("#passwordlength").val() >= 48 || $("#passwordlength").val().length === 0) {
-  //     $("#disabledsubmit").prop("disabled", true);
-  //   }
-  //   if ($("#website_url").val().length === 0) {
-  //     $("#disabledsubmit").prop("disabled", true);
-  //   }
-  //   if ($("#user_email").val().length === 0) {
-  //     $("#disabledsubmit").prop("disabled", true);
-  //   }
-  //   if ($lowercase === 0 && $uppercase === 0 && $numbers === 0 && $special === 0) {
-  //     $("#disabledsubmit").prop("disabled", true);
-  //   }
-  //   if (
-  //     (
-  //       $("#passwordlength").val() >= 6 &&
-  //       $("#passwordlength").val() <= 48 &&
-  //       $("#passwordlength").val().length >= 1 &&
-  //       $("#website_url").val().length > 0 &&
-  //       $("#user_email").val().length > 0
-  //     ) &&
-  //     ($lowercase > 0 || $uppercase > 0 || $numbers > 0 || $special > 0)
-  //   ) {
-  //     $("#disabledsubmit").prop("disabled", false);
-  //   }
-  // });
 });
