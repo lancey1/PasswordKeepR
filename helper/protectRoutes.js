c36b3ce417ee036d24cff5120b44646b8f5d8c43
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-09 17:43:53 -0400

    fix-org-page-protection

--- helper/protectRoutes.js
+++ helper/protectRoutes.js
@@ -8,7 +8,14 @@ const protectAuthRoutes = function (req, res, next) {
     next();
 }
 
+const checkUserPermission = function (req, res, next) {
+    if (res.locals.isAdmin) return next();
+    if (!res.locals.isMember) return res.status(403).render('403');
+    next();
+}
+
 module.exports = {
     protectAdminRoutes,
     protectAuthRoutes,
+    checkUserPermission,
 }
